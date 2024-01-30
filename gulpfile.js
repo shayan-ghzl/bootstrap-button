83625c092822c1c0f4baca163a6f994958185814
shayan ghazali <shayan97.tti@gmail.com>
2024-01-30 18:02:24 +0330

    feat: testability added

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,12 @@ const gulpSass = require('gulp-sass');
 // ----------------------------------------------
 const sass = gulpSass(dartSass);
 
-
 gulp.task("sass", async function () {
     return gulp.src(['./src/button.scss'])
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./dist'));
 });
+
+gulp.task('watch',  function () {
+    gulp.watch('./src/**/*.scss', gulp.series(['sass']));
+  });
